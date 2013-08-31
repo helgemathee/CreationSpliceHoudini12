25b139780c8db190ed61f11c0323610d698767b0
MilanSuk <milansuk86@gmail.com>
2013-08-31 20:23:11 +0200

    Copied install instruction to README file

--- Linux64_gcc4_4/src/SOP_Splice.cpp
+++ Linux64_gcc4_4/src/SOP_Splice.cpp
@@ -7,25 +7,6 @@ This file may not be redistributed in whole or significant part.
 This notice may not be removed or altered from any source distribution.
 */
 
-/*
-Install:
-copy "SOP_Splice.so" file and "RT", "Exts" folders to /home/ubuntu/houdini12.5/dso
-
-run console and write:
-	export FABRIC_RT_PATH=/home/ubuntu/houdini12.5/dso/RT
-	export FABRIC_EXTS_PATH=/home/ubuntu/houdini12.5/dso/Exts
-
-	cd /opt/hfs12.5.469/
-	source houdini_setup
-
-	cd < "path to your install folder" /CreationSpliceHoudini12_linux64_gcc4_4>
-	sh build.sh
-
-	houdini -foreground
-
-Enjoy it!
-*/
-
 #include "SOP_Splice.h"
 
 #define FEC_STATIC
--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 CreationSpliceHoudini12
 =======================
-
 Creation Splice implementation in Houdini 12
 
 
 Install:
 copy "SOP_Splice.so" file and "RT", "Exts" folders to /home/ubuntu/houdini12.5/dso
 
-run console and write:
+Run console and write:
 export FABRIC_RT_PATH=/home/ubuntu/houdini12.5/dso/RT
 export FABRIC_EXTS_PATH=/home/ubuntu/houdini12.5/dso/Exts
 
-cd /opt/hfs<your Houdini version>/
+cd /opt/hfs12.5.469/
 source houdini_setup
 
 cd < "path to your install folder" /CreationSpliceHoudini12_linux64_gcc4_4>
 sh build.sh
 
 houdini -foreground
+
+
 
 
 Enjoy it!
